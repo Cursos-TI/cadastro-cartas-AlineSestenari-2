5ba99e693ac5364a87e53923bfa3c5356be1c137
AlineSestenari <alinesestenari@gmail.com>
2026-04-30 19:09:48 +0000

    Alterado nível para aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,10 +7,10 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-   int carta = 1, Carta =2, populacao1, populacao2, pontos1, pontos2;
-   float area1, area2, PIB1, PIB2;
+   int carta = 1, Carta =2, pontos1, pontos2, populacao1, populacao2;
+   float area1, area2, PIB1, PIB2, densidade1, densidade2, percapita1, percapita2;
    char estado1, estado2;
-   char cidade1[30], cidade2[30];
+   char cidade1[50], cidade2[50];
 
   // Área para entrada de dados
 
@@ -72,6 +72,11 @@ int main() {
                          printf("Área: %2.f km2\n", area1);
                          printf("PIB: %2.f bilhões de reais\n", PIB1);
                          printf("Pontos turísticos: %d\n", pontos1);
+                         densidade1 = (float)(populacao1 / area1);
+                         printf("Densidade populacional: %.2f hab/km²\n", densidade1);
+                         percapita1 = (float)(PIB1 * 1000000000 / populacao1);
+                         printf("PIB per Capita: %.2f reais", percapita1);
+
                          printf("\n");
 
                         // Carta 2
@@ -84,6 +89,11 @@ int main() {
                             printf("Área: %2.f km2\n", area2);
                             printf("PIB: %2.f bilhões de reais\n", PIB2);
                             printf("Pontos turísticos: %d\n", pontos2);
+                            densidade2 = (float)(populacao2 / area2);
+                            printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+                            percapita2 = (float)(PIB2 * 1000000000 / populacao2);
+                            printf("PIB per Capita: %.2f reais", percapita2);
+
 
 return 0;
 } 
